929d68294dc83c81c3341ee9c70622ae093b9528
abouhid <alexandre.bouhid@engenharia.ufjf.br>
2020-09-16 19:01:38 -0300

    sign up account

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_16_212815) do
+ActiveRecord::Schema.define(version: 2020_09_16_215140) do
 
   create_table "tweeets", force: :cascade do |t|
     t.text "tweeet"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
