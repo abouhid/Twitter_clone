baadc397a547be9dbba12ca2cae7af04b983c401
abouhid <alexandre.bouhid@engenharia.ufjf.br>
2020-09-17 11:20:54 -0300

    fixed sign in issue and applied rubocop

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  devise_for :users, :controllers => {registrations: 'registrations'}
+  devise_for :users, controllers: { registrations: 'registrations' }
   resources :tweeets
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'tweeets#index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/migrate/20200916202003_create_tweeets.rb
+++ db/migrate/20200916202003_create_tweeets.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class CreateTweeets < ActiveRecord::Migration[6.0]
   def change
     create_table :tweeets do |t|
